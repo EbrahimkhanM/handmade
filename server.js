cd2773246dfc5a7da763a9b38880e61f1655aac1
EbrahimkhanM <ebrahimkhaanm@gmail.com>
2023-05-26 14:39:33 +0500

    before presentation

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express"); //Line 1
 const app = express(); //Line 2
 var bodyParser = require("body-parser");
-const port = process.env.PORT || 5000; //Line 3
+const port = process.env.PORT || 3000; //Line 3
 var cors = require("cors");
 app.use(cors());
 app.use(bodyParser.json());
@@ -13,7 +13,7 @@ app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6e
 //   //Line 9
 //   res.send({ express: "YOUR EXPRESS BACKEND IS CONNECTED TO REACT" }); //Line 10
 // });
-const stripe = require("stripe")("sk_test_51LBCqSG99swRqIy6ExYJ9htJNINXREcI9j4Tq9n45E1FazYzG2IVjbBLETStg2UoQDUwOh4JAkr7CjE0GUoo5L7800xOc9DoGN");
+const stripe = require("stripe")("sk_test_51NBHJ7DqTIaHg4Baj7dzOqq9HyqnK79Hk8Zuoyocs8pgnpDbw20mNjX25ZdJayTwKmGiIOEfx32KrKLH8CMvIntk00WrLLdrMh");
 
 app.post("/payment", cors(), async (req, res, next) => {
   const token = req.body.stripeToken.id;
